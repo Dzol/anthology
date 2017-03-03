e096f8a9d360759f51444b2ed36f8f2fb97cb53c
Joseph Yiasemides <joseph.yiasemides@erlang-solutions.com>
2017-03-03 13:47:05 -0500

    Introduce predicate list:size/2

--- list.pl
+++ list.pl
@@ -1,10 +1,11 @@
-:- module(list, [test/0, last/2, penultimate/2, element/3]).
+:- module(list, [test/0, last/2, penultimate/2, element/3, size/2]).
 
 test() :-
 
     last(d, [a, b, c, d]),
     penultimate(c, [a, b, c, d]),
-    element(c, [a,b,c,d,e], 3).
+    element(c, [a,b,c,d,e], 3),
+    size(3, [fu, bar, baz]).
 
 last(X, [X]).
 last(X, [_|Tl]) :-
@@ -21,3 +22,9 @@ element(X, [_|Tl], N) :-
 
     M is N - 1,
     element(X, Tl, M).
+
+size(1, [_]).
+size(X, [_|Tl]) :-
+
+    size(Y, Tl),
+    X is Y + 1.
