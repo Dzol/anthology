44dfe120aa6673d4a26029e32ee6208fcd919036
Joseph Yiasemides <joseph.yiasemides@erlang-solutions.com>
2017-03-03 11:26:25 -0500

    Introduce penultimate/2 and element/3

--- list.pl
+++ list.pl
@@ -1,10 +1,23 @@
-:- module(list, [test/0, first/2, last/2]).
+:- module(list, [test/0, last/2, penultimate/2, element/3]).
 
 test() :-
 
-    last(d, [a, b, c, d]).
+    last(d, [a, b, c, d]),
+    penultimate(c, [a, b, c, d]),
+    element(c, [a,b,c,d,e], 3).
 
 last(X, [X]).
-last(X, [Hd|Tl]) :-
+last(X, [_|Tl]) :-
 
     last(X, Tl).
+
+penultimate(X, [X,_]).
+penultimate(X, [_|Tl]) :-
+
+    penultimate(X, Tl).
+
+element(X, [X|_], 1).
+element(X, [_|Tl], N) :-
+
+    M is N - 1,
+    element(X, Tl, M).
