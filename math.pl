dc9e5f3f15a4aaf43d93f0aaf16e73ec9251c3d1
Joseph Yiasemides <joseph.yiasemides@erlang-solutions.com>
2017-03-10 13:30:14 -0500

    Introduce predicate fibonacci/2

--- math.pl
+++ math.pl
@@ -5,11 +5,13 @@
 %% Adapted from Paul Brna's online tutorial
 :- export(successor/2).
 :- export(square/2).
+:- export(fibonacci/2).
 
 test() :-
 
   successor(0, 1),
-  square(2, 4).
+  square(2, 4),
+  fibonacci(5, 5).
 
 successor(X, Y) :-
   number(X), Y is X + 1; number(Y), X is Y - 1.
@@ -23,3 +25,19 @@ sum([_], 1).
 
 sum([H|T], N) :-
   sum(T, M), N is M + H.
+
+fibonacci(0, 0).
+
+fibonacci(1, 1).
+
+fibonacci(N, M) :-
+
+    N > 0,
+
+    I is N - 1,
+    J is N - 2,
+
+    fibonacci(I, X),
+    fibonacci(J, Y),
+
+    M is X + Y.
